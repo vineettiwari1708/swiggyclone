f290c0eb3ba73e3bd2a54e53777a61b9265f67bf
unknown <vineettiwari1708@gmail.com>
2024-12-30 20:36:35 +0530

    root

--- App.js
+++ App.js
@@ -1,3 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
+
+
+// Header
+//      Logo
+//      Nav Item
+// Body
+//      Search
+//      RestaurentCard
+// footer
+//      copyright 
+//      link
+//      Address
+//      Contact
+
+const heading=React.createElement("h1", {id:"heading"},"Vineet Tiwari");
+console.log(heading);
+
+const jsxHeading = <h1 id="heading">My Name is Vineet Tiwari</h1>
+console.log(jsxHeading);
+const root= ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
